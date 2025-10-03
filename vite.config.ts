82de752130dcb49eb3c299e842bbb02586ea2bf5
Stephen Rey Agustinez <agustinez.stephen@gmail.com>
2025-10-03 18:29:07 +0800

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/stpn-resume/', // <-- Added for GitHub Pages
   plugins: [react()],
 })
